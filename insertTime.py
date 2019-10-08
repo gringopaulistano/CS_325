3d8029b8e103caa3348b9763b525e4a5e68433c5
Jamie Loebe <46545560+gringopaulistano@users.noreply.github.com>
2019-10-07 19:41:19 -0500

    Update insertTime.py

--- insertTime.py
+++ insertTime.py
@@ -14,7 +14,7 @@ def insertSort(arr):
 
   for i in range(1, len(arr)): #Loop thru array
     cur = arr[i] #cur contains value of current position in array
-    pos = i - 1 #starts at 1, contains pos of array
+    pos = i - 1 #contains position left of cur
 
     while (pos >= 0) and (arr[pos] > cur): #If value on left larger
       arr[pos+1] = arr[pos] #Move to right
@@ -28,15 +28,15 @@ def main():
     # executing a for loop for each of the element in the lst_size
     for size in lst_sizes:
         
-        arr = createArray(size, 10000)  # will create the array with size and each of the elements in 
-                                        # the range 0 to 9999.
-        # get the start time
+        arr = createArray(size, 10000)  #Create array with size n, values up to 10,000
+                     
+        #start clock
         start_time = timeit.default_timer()    
-        # call the insertion sort
+        #sort
         insertSort(arr)
-        # get the stop time
+        #stop clock
         stop_time = timeit.default_timer()
-        # calculate the time taken
+        #calculate time taken
         time_taken = stop_time - start_time
         print("Array Size:", size, "time taken:", time_taken, "seconds") 
     
